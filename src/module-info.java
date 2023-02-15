eaaad113056c71d866eec747f00cd5824fcffb4d
RupSTLer <rupam.chakraborty@stl.tech>
2023-02-15 11:22:56 +0530

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rupam.chakraborty
+ *
+ */
+module Helloworld {
+}
---
+++ src/rups/hw2.java
@@ -0,0 +1,20 @@
+package rups;
+
+public class hw2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("rups");
+		
+		String str = "RUPAM";
+		
+		
+		for (int i = str.length(); i > 0 ; --i) 
+		{
+			char str2 = str.charAt(i-1);
+			System.out.print(str2);
+		}
+		
+	}
+
+}
